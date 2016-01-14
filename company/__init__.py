aafaeb16586f4cab0fd7143ebc8768995ea5b5c5
Joeny Bui <jbui@protection-consultants.com>
2016-01-13 21:07:56 -0600

    Update version number.

--- company/__init__.py
+++ company/__init__.py
@@ -1,3 +1,12 @@
+__title__ = 'Django Company'
+__version__ = '1.0.0'
+__author__ = 'Joeny Bui'
+__copyright__ = 'Copyright 2011-2016 Joeny Bui'
+
+# Version synonym
+VERSION = __version__
+
+
 USER_COMPANY_ADMIN = 0
 USER_WORKER = 1
 USER_READONLY = 2
