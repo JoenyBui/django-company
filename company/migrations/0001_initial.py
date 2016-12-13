84b1256fe79ec624a3e71d52da474c24dd11249a
jbui <jbui@protection-consultants.com>
2016-12-13 16:08:42 -0600

    Redo migrations.

--- company/migrations/0001_initial.py
+++ company/migrations/0001_initial.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 
 from django.db import models, migrations
 from django.conf import settings
+import uuid
 
 
 class Migration(migrations.Migration):
@@ -15,22 +16,24 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Company',
             fields=[
-                ('id', models.AutoField(primary_key=True, serialize=False, verbose_name='ID', auto_created=True)),
+                ('id', models.AutoField(auto_created=True, primary_key=True, verbose_name='ID', serialize=False)),
+                ('public_id', models.UUIDField(default=uuid.uuid4, editable=False)),
                 ('name', models.CharField(max_length=255)),
-                ('street', models.CharField(default='', max_length=100)),
-                ('city', models.CharField(default='', max_length=100)),
-                ('state', models.CharField(default='', max_length=2)),
+                ('street', models.CharField(max_length=100, default='')),
+                ('city', models.CharField(max_length=100, default='')),
+                ('state', models.CharField(max_length=10, default='')),
                 ('zipcode', models.IntegerField(default=0)),
                 ('website', models.URLField(blank=True)),
-                ('picture', models.ImageField(blank=True, upload_to='profile_images')),
+                ('picture', models.ImageField(upload_to='profile_images', blank=True)),
             ],
         ),
         migrations.CreateModel(
             name='Employee',
             fields=[
-                ('id', models.AutoField(primary_key=True, serialize=False, verbose_name='ID', auto_created=True)),
-                ('privilege', models.IntegerField(default=1, choices=[(0, 'Administrated Access'), (1, 'Write Access'), (2, 'View Access')])),
-                ('company', models.ForeignKey(to='company.Company')),
+                ('id', models.AutoField(auto_created=True, primary_key=True, verbose_name='ID', serialize=False)),
+                ('public_id', models.UUIDField(default=uuid.uuid4, editable=False)),
+                ('privilege', models.IntegerField(choices=[(0, 'Administrated Access'), (1, 'Write Access'), (2, 'View Access'), (3, 'No Access')], default=1)),
+                ('company', models.ForeignKey(default=False, to='company.Company')),
                 ('user', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
             ],
         ),
